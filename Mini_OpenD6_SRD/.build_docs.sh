e22961eafd80d09b42f5c70586eb4a8d6273b48a
SRD-Archive <38624115+SRD-Archive@users.noreply.github.com>
2024-04-24 17:13:54 -0700

    Delete Mini_OpenD6_SRD directory

--- Mini_OpenD6_SRD/.build_docs.sh
+++
@@ -1,6 +0,0 @@
-echo "Mini OpenD6 SRD Document Bilder"
-echo "Building html, docx, and odt files from Markdown."
-
-pandoc -s -t odt -f markdown_strict Mini_OpenD6_SRD.MD -o Mini_OpenD6_SRD.odt
-pandoc -s -t html -f markdown_strict Mini_OpenD6_SRD.MD -o Mini_OpenD6_SRD.html
-pandoc -s -t docx -f markdown_strict Mini_OpenD6_SRD.MD -o Mini_OpenD6_SRD.docx
